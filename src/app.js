aee34d117fd616664efc74c5383225e3edccd609
bryam <layhamshavii@gmail.com>
2025-03-14 11:55:36 +0000

    dominio-bryam

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+const dominio =['com', 'es', 'io', 'net'];
+
+function generateDominio () {
+      for (let pr of pronoun) {
+        for (let ad of adj) {
+          for (let no of noun) {
+            for (let dom of dominio) {
+              console.log(`${pr}${ad}${no}${dom}`);
+            }
+          }
+        }
+      }
+    }
+generateDominio()
+    
